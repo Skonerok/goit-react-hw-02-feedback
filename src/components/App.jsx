d4d6fd93120414f682b35c08bf975401d2085b0a
Oleh Dashkevych <skoner@ukr.net>
2023-04-28 17:29:12 +0300

    finished 1 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,60 @@
-import Statistics from "./Counter/Statistics";
+import { Component } from 'react';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
+import Statistics from './Statisctics';
+import css from './App.module.css';
 
-export const App = () => {
-  return (
-    <div>
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-      <Statistics
+  handleFeedbackButton = ({ target: { name } }) => {
+    this.setState((prevState) => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
 
-      />
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
 
+  countPositiveFeedbackPercentage = () => {
+    const goodFeedback = this.state.good;
+    const totalFeedback = this.countTotalFeedback();
+    return totalFeedback ? Math.round((goodFeedback / totalFeedback) * 100) : 0;
+  }
 
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    const options = Object.keys(this.state);
+    const handleFeedbackButton = this.handleFeedbackButton;
 
+    return (
+      <div className={css.feedback__container}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={handleFeedbackButton}
+            />
+        </Section>
 
-      
-    </div>
-  );
+        <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={totalFeedback}
+          positivePercentage={positivePercentage}
+          />
+        </Section>
+      </div>
+)};
 };
+
+export default App;
